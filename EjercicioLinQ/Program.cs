3d0ba78a7a15d68c2c402b94952715f7b51154b7
CSI20-JCBC1 <csi20-jbadrbo@colegioaltair.net>
2023-01-12 09:29:07 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioLinQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args) { 
+
+       List<int> listaEnteros = new List<int>();
+        for (int i =1; i < 101; i++)
+        {
+            listaEnteros.Add(i);
+            
+        }
+
+       var numeros = from i in listaEnteros where (i % 7 ==0)
+              orderby i descending
+              select i.ToString();
+
+        foreach (var numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+
+        Console.WriteLine();
+
+        LinQuery(listaEnteros);
+    }
+
+    public static void LinQuery(List<int> listaEnteros)
+    {
+
+        var numeros = listaEnteros.Where(a => a % 7 == 0);
+        foreach (var numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+
+    }
+}
+
+
